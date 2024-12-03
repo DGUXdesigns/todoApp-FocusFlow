26f2afa6b799ae31d7db4fe1395b9cba50eacaef
Daveian <[dguxdesigns@gmail.com]>
2024-12-02 22:13:28 -0500

    Add function to check if task is overdue
    - add isOverdue function to check if inputted task date is before current date

--- src/task.js
+++ src/task.js
@@ -1,12 +1,20 @@
 // Task Model
 export class Task {
     // TODO: add checklist and tags
-    constructor(title, description, date, priority) {
+    constructor(title, description, date, priority, tag) {
         this.title = title;
         this.description = description;
         this.date = date;
         this.priority = priority;
+        this.tag = tag;
         this.completed = false // Optional: Tracks task completion
+    }
+
+    isOverdue() {
+        const taskDate = new Date(this.date);
+        const currentDate = new Date();
+
+        return taskDate < currentDate && !this.completed;
     }
 }
 
